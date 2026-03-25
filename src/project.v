1f63bb98c0d37139b51e9cb2e4b282a4743c5e24
Mahmoud Magdy <102227604+mahmoudmq@users.noreply.github.com>
2026-03-25 10:15:30 +0200

    Add memory declaration in project.v

--- src/project.v
+++ src/project.v
@@ -15,7 +15,7 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+    wire [11:0]memory[255:0];
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
